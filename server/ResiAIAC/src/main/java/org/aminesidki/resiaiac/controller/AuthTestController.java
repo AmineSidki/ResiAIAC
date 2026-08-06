6ae3bf25c1c6138448685912411f2de4c1c1f780
Amine Sidki <amineyoutubw@gmail.com>
2026-08-06 16:16:31 +0100

    [FEAT]: Authentication done, all is left is to extract keycloak config

---
+++ server/ResiAIAC/src/main/java/org/aminesidki/resiaiac/controller/AuthTestController.java
@@ -0,0 +1,37 @@
+package org.aminesidki.resiaiac.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("test")
+@RestController
+@RequestMapping("/api/v1/auth-test")
+public class AuthTestController {
+  @GetMapping("/public")
+  public String publicRoute(){
+    return "Hello world !";
+  }
+
+  @GetMapping("/private")
+  public String privateRoute() {
+    return "Hello, you are authenticated !";
+  }
+
+  @PreAuthorize("hasAuthority('ROLE_ADMINISTRATEUR')")
+  @GetMapping("/private/admin")
+  public String privateAdminRoute(){
+    return "Hello, administrator !";
+  }
+
+  @PreAuthorize("hasRole('ADMINISTRATEUR')")
+  @GetMapping("/private/admin/username")
+  public String privateAdminRouteTestCredentialExtraction(@AuthenticationPrincipal Jwt jwt){
+    return "Hello, "+ jwt.getClaimAsString("preferred_username")+" !";
+  }
+}
