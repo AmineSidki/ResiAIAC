5e70d8d74d22814996f8da12c5d1089a17d43cce
YacineDaher <yacine.daher4@gmail.com>
2026-08-12 16:07:26 +0100

    feat:Ajout des services (EquipementUpc, UtilisateurPromotionChambre)

--- server/ResiAIAC/src/main/java/org/aminesidki/resiaiac/service/impl/UtilisateurPromotionChambreServiceImpl.java
+++ server/ResiAIAC/src/main/java/org/aminesidki/resiaiac/service/impl/UtilisateurPromotionChambreServiceImpl.java
@@ -3,13 +3,17 @@ package org.aminesidki.resiaiac.service.impl;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.aminesidki.resiaiac.dto.UtilisateurPromotionChambreDto;
+import org.aminesidki.resiaiac.entity.UtilisateurPromotionChambre;
 import org.aminesidki.resiaiac.entity.id.UtilisateurPromotionChambreId;
 import org.aminesidki.resiaiac.mapper.UtilisateurPromotionChambreMapper;
 import org.aminesidki.resiaiac.repository.UtilisateurPromotionChambreRepository;
 import org.aminesidki.resiaiac.service.UtilisateurPromotionChambreService;
+import org.aminesidki.resiaiac.util.ResourceFetcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class UtilisateurPromotionChambreServiceImpl implements UtilisateurPromotionChambreService {
 
@@ -18,21 +22,40 @@ public class UtilisateurPromotionChambreServiceImpl implements UtilisateurPromot
 
   @Override
   public UtilisateurPromotionChambreDto save(UtilisateurPromotionChambreDto dto) {
-    return null;
+    UtilisateurPromotionChambre entity = utilisateurPromotionChambreMapper.toEntity(dto);
+    utilisateurPromotionChambreRepository.save(entity);
+    return utilisateurPromotionChambreMapper.toDto(entity);
   }
 
+  @Transactional(readOnly = true)
   @Override
   public UtilisateurPromotionChambreDto getById(
       UUID utilisateurId, UUID promotionId, UUID chambreId) {
-    return null;
+    UtilisateurPromotionChambreId id =
+        new UtilisateurPromotionChambreId(utilisateurId, promotionId, chambreId);
+    UtilisateurPromotionChambre entity =
+        ResourceFetcher.fetchResource(
+            id, utilisateurPromotionChambreRepository, "UtilisateurPromotionChambre");
+    return utilisateurPromotionChambreMapper.toDto(entity);
   }
 
   @Override
   public UtilisateurPromotionChambreDto update(
       UtilisateurPromotionChambreId id, UtilisateurPromotionChambreDto dto) {
-    return null;
+    UtilisateurPromotionChambre entity =
+        ResourceFetcher.fetchResource(
+            id, utilisateurPromotionChambreRepository, "UtilisateurPromotionChambre");
+    utilisateurPromotionChambreMapper.updateEntityFromDto(dto, entity);
+    entity = utilisateurPromotionChambreRepository.save(entity);
+    return utilisateurPromotionChambreMapper.toDto(entity);
   }
 
   @Override
-  public void delete(UUID utilisateurId, UUID promotionId, UUID chambreId) {}
+  public void delete(UUID utilisateurId, UUID promotionId, UUID chambreId) {
+    UtilisateurPromotionChambreId id =
+        new UtilisateurPromotionChambreId(utilisateurId, promotionId, chambreId);
+    utilisateurPromotionChambreRepository.delete(
+        ResourceFetcher.fetchResource(
+            id, utilisateurPromotionChambreRepository, "UtilisateurPromotionChambre"));
+  }
 }
