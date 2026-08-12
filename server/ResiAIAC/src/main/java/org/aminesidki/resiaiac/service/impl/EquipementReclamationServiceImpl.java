02fbdc800c02c954a8c4975df8257fa2ca07eadb
Amine Sidki <amineyoutubw@gmail.com>
2026-08-12 15:48:10 +0100

    [FEAT]: Implemented composite id entity's service as a blueprint, added extensive test controller for it

--- server/ResiAIAC/src/main/java/org/aminesidki/resiaiac/service/impl/EquipementReclamationServiceImpl.java
+++ server/ResiAIAC/src/main/java/org/aminesidki/resiaiac/service/impl/EquipementReclamationServiceImpl.java
@@ -3,13 +3,17 @@ package org.aminesidki.resiaiac.service.impl;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.aminesidki.resiaiac.dto.EquipementReclamationDto;
+import org.aminesidki.resiaiac.entity.EquipementReclamation;
 import org.aminesidki.resiaiac.entity.id.EquipementReclamationId;
 import org.aminesidki.resiaiac.mapper.EquipementReclamationMapper;
 import org.aminesidki.resiaiac.repository.EquipementReclamationRepository;
 import org.aminesidki.resiaiac.service.EquipementReclamationService;
+import org.aminesidki.resiaiac.util.ResourceFetcher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class EquipementReclamationServiceImpl implements EquipementReclamationService {
 
@@ -18,19 +22,34 @@ public class EquipementReclamationServiceImpl implements EquipementReclamationSe
 
   @Override
   public EquipementReclamationDto save(EquipementReclamationDto dto) {
-    return null;
+    EquipementReclamation entity = equipementReclamationMapper.toEntity(dto);
+    entity = equipementReclamationRepository.save(entity);
+    return equipementReclamationMapper.toDto(entity);
   }
 
+  @Transactional(readOnly = true)
   @Override
   public EquipementReclamationDto getById(Long equipementId, UUID reclamationId) {
-    return null;
+    EquipementReclamationId id = new EquipementReclamationId(equipementId, reclamationId);
+    EquipementReclamation entity =
+        ResourceFetcher.fetchResource(id, equipementReclamationRepository, "EquipementReclamation");
+    return equipementReclamationMapper.toDto(entity);
   }
 
   @Override
   public EquipementReclamationDto update(EquipementReclamationId id, EquipementReclamationDto dto) {
-    return null;
+    EquipementReclamation entity = ResourceFetcher.fetchResource(
+            id, equipementReclamationRepository, "EquipementReclamation");
+    equipementReclamationMapper.updateEntityFromDto(dto, entity);
+    entity = equipementReclamationRepository.save(entity);
+    return equipementReclamationMapper.toDto(entity);
   }
 
   @Override
-  public void delete(Long equipementId, UUID reclamationId) {}
+  public void delete(Long equipementId, UUID reclamationId) {
+    EquipementReclamationId id = new EquipementReclamationId(equipementId, reclamationId);
+    equipementReclamationRepository.delete(
+        ResourceFetcher.fetchResource(
+            id, equipementReclamationRepository, "EquipementReclamation"));
+  }
 }
